e5c86c111395c552141edf3b57ec376b81cee870
JosueGallegos <jgallegoss@est.ups.edu.ec>
2024-09-27 12:52:35 -0500

    Seleccion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Metodo de ordenamiento Seleccion: ");
+        MetodosOrdenamiento metodosOrdenamiento = new MetodosOrdenamiento();
+        
+        int[] arreglo = {10, 5, 8, 2, 0};
+    
+        metodosOrdenamiento.printArreglo(arreglo);
+        System.out.println("");
+       
+        metodosOrdenamiento.sortBySeleccionDesOrasc(arreglo, true);
+        metodosOrdenamiento.printArreglo(arreglo);
+        System.out.println("");
+
+        int[] arreglos = {10, 5, 8, 2, 0};
+        metodosOrdenamiento.sortBySeleccionDesOrasc(arreglos, false);
+        metodosOrdenamiento.printArreglo(arreglos);
+
+      
+
+       
+
+       
+
+
+        
+        
+        
+        
     }
 }
